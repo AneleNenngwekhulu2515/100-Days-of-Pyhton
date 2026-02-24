086fb70877b071c863b2c4a675cde0a14e982328
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-02-24 11:37:55 +0200

    Tip calculator

--- day-2 python.py
+++ day-2 python.py
@@ -1,3 +1,11 @@
-print("Hello"[0])
+print("Welcome to the tip calculator!")
 
-print(int("123_456_789") + int("345_456_789"))
+bill = float(input("What was your total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?  "))
+people = int(input("How many people to split the bill? "))
+
+total_percent = tip / 100 * bill
+total_bill = bill + total_percent
+total_per_person = round(total_bill / people, 2)
+
+print(f"Each person should pay: ${total_per_person}")
