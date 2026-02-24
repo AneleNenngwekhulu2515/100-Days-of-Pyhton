c05207f2514fbf23eac9fb1eb0866eaf0e80c43c
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-02-24 19:34:41 +0200

    Day 2 Tip Calculator

--- day-2 python.py
+++ day-2 python.py
@@ -8,4 +8,4 @@ total_percent = tip / 100 * bill
 total_bill = bill + total_percent
 total_per_person = round(total_bill / people, 2)
 
-print(f"Each person should pay: ${total_per_person}")
+print(f"Each person should pay: ${total_per_person}" )
