2e21a3a6527fc1302bd9923c26beeb85484d0ac9
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-02-25 16:07:36 +0200

    Day 5 PyPassword Generator

---
+++ day-5 python.py
@@ -0,0 +1,40 @@
+lst = []
+
+for i in range(1, 101):
+
+    if i % 3 ==0 and i%5==0:
+        print("Fizzbuzz")
+
+    elif i%5==0:
+        print("Buzz")
+    elif i%3==0:
+        print("Fizz")
+    else:
+        print(i)
+
+alphabet = [
+    'a','b','c','d','e','f','g','h','i','j',
+    'k','l','m','n','o','p','q','r','s','t',
+    'u','v','w','x','y','z',
+    'A','B','C','D','E','F','G','H','I','J',
+    'K','L','M','N','O','P','Q','R','S','T',
+    'U','V','W','X','Y','Z'
+]
+
+numbers = ['1','2','3','4','5','6','7','8','9','10']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+num_letters = int(input("How many letters would you like in your password? \n "))
+num_symbols = int(input("How many symbols would you like in your password? \n "))
+num_numbers = int(input("How many numbers would you like in your password? \n "))
+
+
+
+for i in range(num_letters):
+    print(i)
+
+
+
+
+
