a2cf7f61d9b6d5d022325624715bc3f25600fb8a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-09 17:46:51 +0200

    Day 10

--- day-10 python.py
+++ day-10 python.py
@@ -1,7 +1,7 @@
 def format_name(f_name, l_name):
     formatted_f_name = f_name.title()
     formatted_l_name = l_name.title()
-    return f"{formattes_f_name} {formatted_l_name}"
+    return f"{formatted_f_name} {formatted_l_name}"
 
 formatted_string = format_name("anele", "nenngwekhulu")
 print(formatted_string)
