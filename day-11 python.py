49ed72c96993ba818fd1968fda6173b3cf2f0e98
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-12 19:01:09 +0200

    Day 11 BlackJack

--- day-11 python.py
+++ day-11 python.py
@@ -72,13 +72,13 @@ while not is_game_over:
     computer_score = calculate_score(computer_cards)
 
     print(f"Your cards: {user_cards}, current score: {user_score}")
-    print(f"Computer cards: {computer_cards[0]}")
+    print(f"Computer's first card: {computer_cards[0]}")
 
 
     if user_score>21 or user_score==0 or computer_score==0:
         is_game_over = True
     else:
-        deal = input("Do you want to draw another card? 'y' or 'n' ").lower()
+        deal = input("Do you want to draw another card? 'y' or 'n': ").lower()
         if deal == "y":
             drawed_card = deal_card()
             user_cards.append(drawed_card)
@@ -86,19 +86,19 @@ while not is_game_over:
         elif deal == "n":
             is_game_over = True
 
-    if computer_score<17:
-        computer_drawed_card = deal_card()
-        computer_cards.append(computer_drawed_card)
-        computer_score = calculate_score(computer_cards)
+while computer_score<17 and computer_score != 0:
+    computer_drawed_card = deal_card()
+    computer_cards.append(computer_drawed_card)
+    computer_score = calculate_score(computer_cards)
 
-    print(f"Your cards: {user_cards}, current score: {user_score}")
-    print(user_cards)
+    # print(f"Computer cards: {computer_cards[0]}")
+    # print(user_cards)
 
-    def compare(user_score, computer_score):
-        if user_score==computer_score:
-            return "Its a Draw"
-        if computer_score == 0:
-            return "You loose"
+def compare(user_score, computer_score):
+    if user_score==computer_score:
+        return "Its a Draw"
+    if computer_score == 0:
+        return "You loose"
 
 
 
