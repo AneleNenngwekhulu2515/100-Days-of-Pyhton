89b853154fa2fe3329c711ceb7fffd2610dd5a72
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-16 16:04:28 +0200

    Day 14 Higher or Lower Game

--- day-14 python/game.py
+++ day-14 python/game.py
@@ -5,7 +5,7 @@ print(r""" +-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+
  |H|i|g|h|e|r| |o|r| |L|o|w|e|r|
  +-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+""")
 
-individual_a =random.choice(list(game_data.celebrities))
+individual_a =random.choice(game_data.celebrities)
 print(f"Compare A: {individual_a['name']}, {individual_a['description']}, {individual_a['country']}")
 game_data.celebrities.remove(individual_a)
 print(r""" _  _  ___ 
@@ -13,7 +13,11 @@ print(r""" _  _  ___
  \  / \__ \
   \/  (___/""")
 
-individual_b = random.choice(list(game_data.celebrities))
+individual_b = random.choice(game_data.celebrities)
+
+while individual_b == individual_a:
+    individual_b = random.choice(game_data.celebrities)
+
 print(f"Against B: {individual_b['name']}, {individual_b['description']}, {individual_b['country']}")
 game_data.celebrities.remove(individual_b)
 guess = input("Who has more followers? Type 'A' or 'B': ").upper()
@@ -21,13 +25,31 @@ guess = input("Who has more followers? Type 'A' or 'B': ").upper()
 score = 0
 
 
-people = individual_a['followers'],individual_b['followers']
-print(people)
+if individual_a["followers"] > individual_b["followers"]:
+    correct_answer = "A"
+else:
+    correct_answer = "B"
 
 # if people[0] > people[1]:
 #     print(f"{individual_a['name']} wins!")
 # else:
 #     print(f"{individual_a['name']} wins!")
+if guess == correct_answer:
+    score += 1
+    print(f"You're right! Score: {score}")
+else:
+    print(f"Sorry, that's wrong. Final score: {score}")
+
+game_over = False
+score = 0
+
+while not game_over:
+
+    if guess == correct_answer:
+        score += 1
+    else:
+        game_over = True
+
 
 
 
