f6b8c8bbdc71cff7febfd31f3b63395b6ee61276
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-16 12:52:48 +0200

    Day 14 Higher or Lower Game

--- day-14 python/game.py
+++ day-14 python/game.py
@@ -1,5 +1,9 @@
+import game_data
+
 print(r""" +-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+
  |H|i|g|h|e|r| |o|r| |L|o|w|e|r|
  +-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+""")
 
+print(game_data.celebrities[1])
 
+def compare():
---
+++ day-14 python/game_data.py
@@ -0,0 +1,18 @@
+celebrities = [
+    {"name":"Cristiano Ronaldo", "followers":1000000000, "desc":"Football superstar", "country":"Portugal"},
+    {"name":"Lionel Messi", "followers":500000000, "desc":"Football legend", "country":"Argentina"},
+    {"name":"Selena Gomez", "followers":415000000, "desc":"Singer & actress", "country":"USA"},
+    {"name":"Kylie Jenner", "followers":390000000, "desc":"Media personality", "country":"USA"},
+    {"name":"Dwayne Johnson", "followers":390000000, "desc":"Actor & wrestler", "country":"USA"},
+    {"name":"Ariana Grande", "followers":372000000, "desc":"Pop star", "country":"USA"},
+    {"name":"Kim Kardashian", "followers":353000000, "desc":"Entrepreneur", "country":"USA"},
+    {"name":"Beyoncé", "followers":308000000, "desc":"Music icon", "country":"USA"},
+    {"name":"Khaby Lame", "followers":160300000, "desc":"TikTok influencer", "country":"Senegal/Italy"},
+    {"name":"Juliano Floss", "followers":20000000, "desc":"Dancer & influencer", "country":"Brazil"},
+    {"name":"Akamz", "followers":23000000, "desc":"Social comedian", "country":"France"},
+    {"name":"Jennifer Lopez", "followers":247700000, "desc":"Singer & actress", "country":"USA"},
+    {"name":"Katy Perry", "followers":202700000, "desc":"Pop singer", "country":"USA"},
+    {"name":"Kevin Hart", "followers":176100000, "desc":"Comedian & actor", "country":"USA"},
+    {"name":"Cardi B", "followers":163700000, "desc":"Rapper & star", "country":"USA"}
+]
+
