17141f74400d44f1c955511a50f0cdb65b701b3a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-17 12:20:25 +0200

    Day 15 Coffee Machine

--- day-15 python/main.py
+++ day-15 python/main.py
@@ -1,20 +1,34 @@
 from coffee_machine import MENU, resources
 
-coffee_maker = False
+coffee_maker = True
 
-def money():
-    penny = 0.01
-    nickel =0.05
-    dime = 0.10
-    quater = 0.25
+def money(money_recieved, drink_cost):
+    # penny = 0.01
+    # nickel =0.05
+    # dime = 0.10
+    # quater = 0.25
+    if money_recieved >= drink_cost:
+        change = round(money_recieved - drink_cost, 2)
+        print(f"Here is ${change} in change")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry not enough money. Money refunded")
+        return False
 
 
-while not coffee_maker:
+while coffee_maker:
+
     drink = input("What would you like? (espresso/latte/cappucino): ").lower()
+    if drink == "off":
+        coffee_maker = False
 
     if drink == "report":
-        for key,value in resources.items():
-            print(f"{key}: {value}")
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}ml")
+        print(f"Money: {resources['water']}ml")
 
     if drink == "espresso":
         for key, value in MENU["espresso"]["ingredients"].items():
