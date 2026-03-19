8cfa20fc558db11b97009dbb88119352bb90f98c
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-19 10:58:28 +0200

    Day 16 OOP Turtle

---
+++ day-16 python.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("dark violet")
+
+
+my_screen = Screen()
+print(my_screen.canvheight)
+#will alow the program to continue running until we click on the screen
+my_screen.exitonclick()
