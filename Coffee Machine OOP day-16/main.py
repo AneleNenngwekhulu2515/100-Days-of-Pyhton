bad7581e73e7060d6d201050ee454da93a68a782
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-20 11:08:16 +0200

    Day 16 OOP Coffee Machine

--- Coffee Machine OOP day-16/main.py
+++ Coffee Machine OOP day-16/main.py
@@ -3,18 +3,29 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 menu = Menu()
+money = MoneyMachine()
+coffee_maker = CoffeeMaker()
 coffee_machine = True
 
 while coffee_machine:
     drink = input("What would you like? (espresso/latte/cappucino): ").lower()
     if drink == "off":
         coffee_machine = False
+
     if drink == "report":
-        coffee_maker = CoffeeMaker()
-        coffee_maker.report()
-        print(coffee_maker)
-    if drink == menu.find_drink(drink):
+        print(coffee_maker.report())
+        print(money.report())
+
+    found_drink = menu.find_drink(drink)
+
+    if found_drink:
         print("Welcome to the Coffee Machine!")
+        money.process_coins()
+        money.make_payment(found_drink.cost)
+
+
+    else:
+        print("Sorry that item is not available.")
 
 
 
