da060154c04875a7a1cfdcb2158f982f13b07f19
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-20 10:17:44 +0200

    Day 16 OOP Coffee Machine

--- Coffee Machine OOP day-16/main.py
+++ Coffee Machine OOP day-16/main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_machine = True
+
+while coffee_machine:
+    drink = input("What would you like? (espresso/latte/cappucino): ").lower()
+    if drink == "off":
+        coffee_machine = False
+    if drink == "report":
+        coffee_maker = CoffeeMaker()
+        coffee_maker.report()
+        print(coffee_maker)
+    if drink == menu.find_drink(drink):
+        print("Welcome to the Coffee Machine!")
+
+
+
+
+
+
