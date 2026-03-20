4a25d809851b879c83e6f48dce2eb958aae3615a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-20 09:28:52 +0200

    Day 16 OOP Turtle

--- day-16 python.py
+++ day-16 python.py
@@ -1,13 +1,22 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("dark violet")
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# #will alow the program to continue running until we click on the screen
+# my_screen.exitonclick()
 
+from prettytable import PrettyTable
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("dark violet")
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu","Squirtle","Charmander"])
+table.add_column("Type",["Electric","Water","Fire"])
+table.align = "l"
 
-
-my_screen = Screen()
-print(my_screen.canvheight)
-#will alow the program to continue running until we click on the screen
-my_screen.exitonclick()
+print(table)
