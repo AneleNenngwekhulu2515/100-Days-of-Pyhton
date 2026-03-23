bf9f2a2bda000498b6b357c5649faab8a93e4e31
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-23 20:25:46 +0200

    Day 17 Quiz Project

--- day-17 python/quiz_brain.py
+++ day-17 python/quiz_brain.py
@@ -5,13 +5,20 @@ class QuizBrain:
         self.question_list = q_list
 
     def next_question(self):
-        current_question = self.question_list[self.questions_number]
+        current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
 
     def still_has_questions(self):
-        if self.question_number < len(self.question_list):
-            return True
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if user_answer.lower() == correct_answer.lower():
+            print("You got it right")
         else:
-            False
+            print("Sorry, that's wrong.")
+        print(f"The correct answer is {correct_answer}")
+
+
 
