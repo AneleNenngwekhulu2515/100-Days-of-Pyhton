ebfedbb68dbbc23e5d5f090fa448c7a4374a5940
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-23 20:30:31 +0200

    Day 17 Quiz Project

--- day-17 python/quiz_brain.py
+++ day-17 python/quiz_brain.py
@@ -2,6 +2,7 @@
 class QuizBrain:
     def __init__(self,q_list):
         self.question_number = 0
+        self.score = 0
         self.question_list = q_list
 
     def next_question(self):
@@ -15,10 +16,12 @@ class QuizBrain:
 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
+            self.score += 1
             print("You got it right")
         else:
             print("Sorry, that's wrong.")
         print(f"The correct answer is {correct_answer}")
+        print(f"Your current score is: {self.score}/{self.question_number}")
 
 
 
