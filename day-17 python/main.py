e953fe4d17181f39909761b3fcf69d2e09c5176c
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-23 20:05:30 +0200

    Day 17 Quiz Project

--- day-17 python/main.py
+++ day-17 python/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 
@@ -8,5 +9,8 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
 
 
--- day-17 python/quiz_brain.py
+++ day-17 python/quiz_brain.py
@@ -1,15 +1,16 @@
-from question_model import *
-from main import *
-import random
-# question = Question()
 
 class QuizBrain:
     def __init__(self,q_list):
-        self.questions_number = 0
+        self.question_number = 0
         self.question_list = q_list
 
     def next_question(self):
         current_question = self.question_list[self.questions_number]
-        input(f"Q.{self.questions_number}: {current_question.text} (True/False): ")
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
 
+    def still_has_questions(self):
+        if self.question_number < len(self.question_list)
+            return True
+        else:
+            False
 
