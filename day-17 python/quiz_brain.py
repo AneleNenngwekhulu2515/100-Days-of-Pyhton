6595ef0ae7d64b2a143b2ff88fdff560a7911c1a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-23 12:14:52 +0200

    Day 17 Quiz Project

--- day-17 python/quiz_brain.py
+++ day-17 python/quiz_brain.py
@@ -9,4 +9,7 @@ class QuizBrain:
         self.question_list = q_list
 
     def next_question(self):
+        current_question = self.question_list[self.questions_number]
+        input(f"Q.{self.questions_number}: {current_question.text} (True/False): ")
+
 
