a445a1d36e3724a382c0aa79084e60cecc12dfef
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-23 11:49:59 +0200

    Day 17 Quiz Project

---
+++ day-17 python/quiz_brain.py
@@ -0,0 +1,9 @@
+from question_model import *
+from main import *
+import random
+# question = Question()
+
+class QuizBrain:
+    def __init__(self, questions, answers):
+        self.questions = questions
+        self.answers = answers
