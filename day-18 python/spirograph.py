e59000dbc8381a27a9716b83b8832f6a61e39d1b
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-24 11:23:06 +0200

    Day 18 Turtle and Tkinter

---
+++ day-18 python/spirograph.py
@@ -0,0 +1,21 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+screen = t.Screen()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+
+tim.speed("fastest")
+tim.circle(100)
+
+
+screen.exitonclick()
+
