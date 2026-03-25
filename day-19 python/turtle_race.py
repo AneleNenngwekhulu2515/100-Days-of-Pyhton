6ad0a0ba4c5017052a36c8d036a054c7c13d5538
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-25 21:38:15 +0200

    Day 19 Turtle and Tkinter

--- day-19 python/turtle_race.py
+++ day-19 python/turtle_race.py
@@ -26,13 +26,22 @@ screen = Screen()
 
 screen.setup(500, 400)
 user_bet = screen.textinput(title="Make your bet!", prompt="Which turtle do you think will win the race? Enter a color:  ")
+y_positions = [0, 100, -100, 50, -50, 20]
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
+
+for turtle_index in range(0,6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 
-tim.goto(x=-230, y=0)
-tom.goto(x=-230, y=100)
-sam.goto(x=-230, y=-100)
-hank.goto(x=-230, y=-50)
-rex.goto(x=-230, y=50)
+# tim.goto(x=-230, y=0)
+# tom.goto(x=-230, y=100)
+# sam.goto(x=-230, y=-100)
+# hank.goto(x=-230, y=-50)
+# rex.goto(x=-230, y=50)
 
 is_race_on = False
 
@@ -40,7 +49,8 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-    random.randint(0, 10)
+    rand_distance = random.randint(0, 10)
+    turtle.forward(rand_distance)
 
 
 screen.exitonclick()
