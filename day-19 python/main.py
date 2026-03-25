8512b573f62c5a83c92c167d790c7d712368c41a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-25 20:25:46 +0200

    Day 19 Turtle and Tkinter

--- day-19 python/main.py
+++ day-19 python/main.py
@@ -3,8 +3,19 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
-def move_forward():
-    tim.forward(10)
+def move_forwards():
+    tim.forward(100)
+
+def move_backwards():
+    tim.backward(100)
+
+def counter_clockwise():
+    tim.circle()
+
+
 
 screen.listen()
-screen.onkey(key="space", fun= move_forward)
+screen.onkey(key="w", fun= move_forwards)
+screen.onkey(key="s", fun= move_backwards)
+screen.onkey(key="a", fun= counter_clockwise)
+screen.exitonclick()
