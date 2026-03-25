a5038fb9f1b7a15071be2a2c753244445b2d8ed1
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-25 21:48:51 +0200

    Day 19 Turtle and Tkinter

--- day-19 python/turtle_race.py
+++ day-19 python/turtle_race.py
@@ -1,25 +1,25 @@
 from turtle import *
 import random
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.color("purple")
-
-tom = Turtle(shape="turtle")
-tom.penup()
-tom.color("red")
-
-sam =Turtle(shape="turtle")
-sam.penup()
-sam.color("green")
-
-hank = Turtle(shape="turtle")
-hank.color("blue")
-hank.penup()
-
-rex = Turtle(shape="turtle")
-rex.color("pink")
-rex.penup()
+# tim = Turtle(shape="turtle")
+# tim.penup()
+# tim.color("purple")
+#
+# tom = Turtle(shape="turtle")
+# tom.penup()
+# tom.color("red")
+#
+# sam =Turtle(shape="turtle")
+# sam.penup()
+# sam.color("green")
+#
+# hank = Turtle(shape="turtle")
+# hank.color("blue")
+# hank.penup()
+#
+# rex = Turtle(shape="turtle")
+# rex.color("pink")
+# rex.penup()
 
 screen = Screen()
 
@@ -31,11 +31,11 @@ colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 all_turtles = []
 
 for turtle_index in range(0,6):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.color(colors[turtle_index])
-    tim.goto(x=-230, y=y_positions[turtle_index])
-
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[turtle_index])
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
 # tim.goto(x=-230, y=0)
 # tom.goto(x=-230, y=100)
@@ -49,8 +49,18 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-    rand_distance = random.randint(0, 10)
-    turtle.forward(rand_distance)
+
+    for turtle in all_turtles:
+        if turtle.xcor()>230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You lost! The {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
 
 
 screen.exitonclick()
