480aa5b77805eabf3193a01216d8be707a1e1465
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-26 18:57:50 +0200

    Day 20 Snake Game

--- day-20 snake_game/main.py
+++ day-20 snake_game/main.py
@@ -1,5 +1,5 @@
 from turtle import Turtle, Screen
-
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -20,8 +20,15 @@ for position in starting_positions:
 game_is_on = True
 
 while game_is_on:
-    for seg in segment:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range( len(segment)-1, 0  ,-1):
+        new_x = segment[seg_num - 1].xcor()
+        new_y = segment[seg_num - 1].ycor()
+        segment[seg_num].goto(new_x, new_y)
+
+
 
 
 
