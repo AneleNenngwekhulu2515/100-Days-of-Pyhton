df5d31f051710f4389d574812ce8529d20fc93e3
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-26 19:33:47 +0200

    Day 20 Snake Game

--- day-20 snake_game/snake.py
+++ day-20 snake_game/snake.py
@@ -8,6 +8,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
@@ -15,11 +16,23 @@ class Snake:
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
-            self.segment.append(new_segment)
+            self.segments.append(new_segment)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
