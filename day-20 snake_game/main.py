dde247d3f3520912870aabb770997991b9c54623
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-26 18:20:28 +0200

    Day 20 Snake Game

--- day-20 snake_game/main.py
+++ day-20 snake_game/main.py
@@ -6,15 +6,17 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
-t = Turtle(shape="square")
-t.color("white")
-s = Turtle(shape="square")
-s.color("white")
-p = Turtle(shape="square")
-p.color("white")
+segment_1 = Turtle(shape="square")
+segment_1.color("white")
 
-t.goto(x=-1, y=0)
-s.goto(x=-2, y=0)
+segment_2 = Turtle(shape="square")
+segment_2.color("white")
+
+segment_3 = Turtle(shape="square")
+segment_3.color("white")
+
+segment_2.goto(x=-20, y=0)
+segment_3.goto(x=-40, y=0)
 
 
 
