da0dab62e42f1fe6a2e8cdca5fabcd13e0156227
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-03-31 14:26:00 +0200

    Day 21 Snake Game

--- day-20 snake_game/main.py
+++ day-20 snake_game/main.py
@@ -12,6 +12,7 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 
+
 screen.listen()
 screen.onkey(snake.up,"Up")
 screen.onkey(snake.down,"Down")
@@ -25,6 +26,10 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+
+    #collision with food
+    if snake.head.distance(food) <15:
+        food.refresh()
 
 
 
