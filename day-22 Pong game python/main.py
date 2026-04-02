a9f29267bc6a818bb53830a251d9a48951dca90a
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-04-02 11:58:08 +0200

    Day 22 Pong Game

--- day-22 Pong game python/main.py
+++ day-22 Pong game python/main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
 import time
 
 screen = Screen()
@@ -10,6 +11,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 
 screen.listen()
@@ -21,8 +23,9 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    # time.sleep(0.1)
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 screen.exitonclick()
 
