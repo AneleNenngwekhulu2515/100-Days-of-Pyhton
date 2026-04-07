eb7f4265d74f4d4f902cab0360530cd0d2eaa8f1
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-04-07 14:10:49 +0200

    Day 23 Turtle Crossing Game

--- day-23 Turtle Crossing Game/main.py
+++ day-23 Turtle Crossing Game/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+screen.listen()
+screen.onkey()
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    player = Player()
+    player.move()
+
+
+
+screen.exitonclick()
--- day-23 Turtle Crossing Game/player.py
+++ day-23 Turtle Crossing Game/player.py
@@ -1,7 +1,21 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.shape("turtle")
+
+    def move(self):
+        # STARTING_POSITION
+        self.forward(MOVE_DISTANCE)
+        if self.ycor() > FINISH_LINE_Y:
+            print("You are at the finish line ")
+            # STARTING_POSITION
+
+
