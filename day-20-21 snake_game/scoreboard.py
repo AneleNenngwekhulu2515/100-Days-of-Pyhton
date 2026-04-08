cb147099d5d0c675f86971893b8da6d13cef55f2
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-04-08 11:14:28 +0200

    Day 24 Updating Snake Game

--- day-20-21 snake_game/scoreboard.py
+++ day-20-21 snake_game/scoreboard.py
@@ -5,7 +5,7 @@ class ScoreBoard(Turtle):
         super().__init__()
         self.score = 0
         with open("data.txt")as data:
-            int(data.read())
+            self.highscore = int(data.read())
         self.goto(0, 270)
         self.color("white")
         self.hideturtle()
@@ -19,6 +19,8 @@ class ScoreBoard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt",mode="w") as data:
+                data.write(f"{self.highscore}")
         self.score = 0
         self.update_scoreboard()
 
