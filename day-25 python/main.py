2aaaf28721a2bb11654baa772872a2c1ecf159b5
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-04-08 13:45:59 +0200

    Day 25 CSV and Pandas

---
+++ day-25 python/main.py
@@ -0,0 +1,19 @@
+# lst = []
+#
+# with open("weather_data - Sheet1.csv") as data:
+#     contents  = data.readlines()
+#     lst.append(contents)
+#     print(lst)
+
+import csv
+import pandas
+
+with open("weather_data - Sheet1.csv") as data_file:
+    data = csv.reader(data_file)
+    temperature = []
+    for row in data:
+        if row[1] != "temp":
+            temperature.append(int(row[1]))
+    print(temperature)
+
+
