776f3d0423304f1e12aa1970a7b812cfeb9d76bf
Anele Nenngwekhulu <nenngwekhuluanele@gmail.com>
2026-04-09 14:52:16 +0200

    Day 25 CSV and Pandas

--- day-25 python/main.py
+++ day-25 python/main.py
@@ -8,12 +8,16 @@
 import csv
 import pandas
 
-with open("weather_data - Sheet1.csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int(row[1]))
-    print(temperature)
+# with open("weather_data - Sheet1.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+data = pandas.read_csv("weather_data - Sheet1.csv")
+print(data["temp"])
+#return
 
 
